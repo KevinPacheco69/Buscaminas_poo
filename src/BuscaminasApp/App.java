c4a4ed146d6d9fb056810467765763a7eb120a53
cucuy <cucuy@DESKTOP-H28JQS9.lan>
2025-12-03 08:34:20 -0500

    App: Aplicacion main para ejecutar el programa

---
+++ src/BuscaminasApp/App.java
@@ -0,0 +1,12 @@
+package BuscaminasApp;
+
+
+import controller.GameController;
+
+public class App {
+
+    public static void main(String[] args) {
+        GameController controlador = new GameController();
+        controlador.run();
+    }
+}
